e4218b59f93a3c69f578a81869f9a93c158b75fb
Pradip Nikam <nikampradip863@gmail.com>
2025-03-26 23:23:58 +0530

    Modified User.java and added UserRepo.java

--- spring-boot-rest/src/main/java/com/ex/learn/model/User.java
+++ spring-boot-rest/src/main/java/com/ex/learn/model/User.java
@@ -1,4 +1,12 @@
 package com.ex.learn.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+     private int id;
+     private String username;
+     private String password;
 }
